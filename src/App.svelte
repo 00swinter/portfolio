d8cd26842e71815268722af34531b137a1afac39
00swinter <rasmus.klaukien@gmail.com>
2026-01-09 19:37:45 +0100

    new page

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,12 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import MainPage from './pages/MainPage.svelte'
-
-
-
-
-
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import MainPage from "./pages/MainPage.svelte";
 </script>
 
-<main>
-<MainPage></MainPage>
 
-</main>
+<MainPage> </MainPage>
+
+
 <style>
-  
 </style>
